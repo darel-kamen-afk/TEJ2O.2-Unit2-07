0b9cf22b8a26f583e0fd7a16ae7a2d66c859e34c
Darel.Grace Kamen <darel.grace-kamen@stu.ocsb.ca>
2026-03-04 22:00:26 -0500

    I made cookies

--- main.ts
+++ main.ts
@@ -1,8 +1,34 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Grace
+ * Created on: Feb 2026
+ * This program will be a cookie clicker 
 */
 
-basic.showString('Hello, World!')
+let cookies = 0
+
+// This shows the 'Cookie' icon from your teacher's example
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        # . . # .
+        # . . # .
+        # . . # .
+        . # # . .
+        `)
+})
+
+// This adds a cookie and shows the number (like your image 'a')
+input.onButtonPressed(Button.A, function () {
+    cookies += 1
+    basic.showNumber(cookies)
+})
+
+// Initialize the screen with the cookie icon
+basic.showLeds(`
+    . # # . .
+    # . . # .
+    # . . # .
+    # . . # .
+    . # # . .
+    `)
